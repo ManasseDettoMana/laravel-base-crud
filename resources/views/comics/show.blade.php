39692cb918c3ab5848f39ad9d5676f634e6325e1
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-11-09 23:07:46 +0100

    agg class show-container / extends main

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,9 +1,12 @@
+@extends('layout.main')
+<div class="show-container">
 
-<a href="{{route('comics.index')}}">< Back to comics</a>
-
-<div>
-    <h2>{{$comic->title}}</h2>
-    <img src="{{$comic->url}}" width="200" alt="">
-    <h3><small>author: </small>{{$comic->author}}</h3>
-    <p>{{$comic->description}}</p>
+    <a href="{{route('comics.index')}}">< Back to comics</a>
+    
+    <div>
+        <h2>{{$comic->title}}</h2>
+        <img src="{{$comic->url}}" width="200" alt="">
+        <h3><small>author: </small>{{$comic->author}}</h3>
+        <p>{{$comic->description}}</p>
+    </div>
 </div>
