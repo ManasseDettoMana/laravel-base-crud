d48571b441b0c814ae5cbaf31b1f763f29b136ed
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-11-09 22:31:19 +0100

    extends main

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,13 +1,12 @@
 <h1>asdas</h1>
-
-@foreach($comics as $comic)
-    <div>
-        <h2>{{$comic->title}}</h2>
-        <img src="{{$comic->url}}" width="200" alt="">
-        <a href="{{route('comics.show', ['id'=>$comic->id])}}">Read More</a>
-        <h3>{{$comic->author}}</h3>
-        <!-- <p>{{$comic->description}}</p> -->
-    </div>
-    
-
-@endforeach
+@extends('layout.main')
+@section('content')
+    @foreach($comics as $comic)
+        <div>
+            <h2>{{$comic->title}}</h2>
+            <img src="{{$comic->url}}" width="200" alt="">
+            <a href="{{route('comics.show', ['id'=>$comic->id])}}">Read More</a>
+            <h3>{{$comic->author}}</h3>
+        </div>       
+    @endforeach  
+@endsection
