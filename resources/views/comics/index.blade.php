ca1c40412702d5ad618af90f7a76c85a36edff78
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-11-09 22:01:22 +0100

    index popolato

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,10 +1,13 @@
 <h1>asdas</h1>
 
 @foreach($comics as $comic)
-    <p>{{$comic->title}}</p>
-    <p>{{$comic->author}}</p>
-    <p>{{$comic->description}}</p>
+    <div>
+        <h2>{{$comic->title}}</h2>
+        <img src="{{$comic->url}}" width="200" alt="">
+        <a href="{{route('comics.show', ['id'=>$comic->id])}}">Read More</a>
+        <h3>{{$comic->author}}</h3>
+        <!-- <p>{{$comic->description}}</p> -->
+    </div>
     
-    <img src="{{$comic->url}}" alt="">
 
 @endforeach
