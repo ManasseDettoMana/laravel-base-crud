feee785ebb83ee78a5f26b5ed1e19be23b99c753
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-11-09 22:43:16 +0100

    img messa allinterno dell'a

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,11 +1,13 @@
-<h1>asdas</h1>
+
 @extends('layout.main')
 @section('content')
     @foreach($comics as $comic)
         <div>
             <h2>{{$comic->title}}</h2>
-            <img src="{{$comic->url}}" width="200" alt="">
-            <a href="{{route('comics.show', ['id'=>$comic->id])}}">Read More</a>
+            
+            <a href="{{route('comics.show', ['id'=>$comic->id])}}">
+                <img src="{{$comic->url}}" width="200" alt="">
+            </a>
             <h3>{{$comic->author}}</h3>
         </div>       
     @endforeach  
