9def4cf4024e94c90dea66f5671b5029e5a6bca1
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-11-10 17:35:12 +0100

    aggiunto container-wrap

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,15 +1,16 @@
 
 @extends('layout.main')
 @section('content')
-    
-    @foreach($comics as $comic)
-        <div class="index-container">
-            <h2>{{$comic->title}}</h2>
-            
-            <a href="{{route('comics.show', ['id'=>$comic->id])}}">
-                <img src="{{$comic->url}}" width="200" alt="">
-            </a>
-            <h3>{{$comic->author}}</h3>
-        </div>       
-    @endforeach  
+    <div class="index-container-wrap">
+        @foreach($comics as $comic)
+            <div class="index-container">
+                <h2>{{$comic->title}}</h2>
+                
+                <a href="{{route('comics.show', $comic)}}">
+                    <img src="{{$comic->url}}" width="200" alt="">
+                </a>
+                <h3>{{$comic->author}}</h3>
+            </div>       
+        @endforeach  
+    </div>
 @endsection
