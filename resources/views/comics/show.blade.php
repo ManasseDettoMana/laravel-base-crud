10dd2ace65a23a9189aaf3330f8a7fa22d3dcd14
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-11-10 17:34:30 +0100

    aggiunto container e section 'content'

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,12 +1,15 @@
 @extends('layout.main')
-<div class="show-container">
-
-    <a href="{{route('comics.index')}}">< Back to comics</a>
+@section('content')
     
-    <div>
-        <h2>{{$comic->title}}</h2>
-        <img src="{{$comic->url}}" width="200" alt="">
-        <h3><small>author: </small>{{$comic->author}}</h3>
-        <p>{{$comic->description}}</p>
+    <div class="show-container">
+
+        <a href="{{route('comics.index')}}">< Back to comics</a>
+        
+        <div>
+            <h2>{{$comic->title}}</h2>
+            <img src="{{$comic->url}}" width="200" alt="">
+            <h3><small>author: </small>{{$comic->author}}</h3>
+            <p>{{$comic->description}}</p>
+        </div>
     </div>
-</div>
+@endsection
